00878f21118455fab6c64452c900de76ff6ae40c
Kritika <rkritika1508@gmail.com>
2018-02-25 17:09:39 +0530

    Finished

--- Score-Keeper-WDB/score.js
+++ Score-Keeper-WDB/score.js
@@ -1,7 +1,11 @@
 var p1Button = document.querySelector("#p1");
 var p2Button = document.querySelector("#p2");
+var resetButton = document.querySelector("#reset");
 var p1Display = document.querySelector("#p1Display");
 var p2Display = document.querySelector("#p2Display");
+var paragraph = document.querySelector("p");
+var numInput = document.querySelector("input");
+var winningScoreDisplay = document.querySelector("p span");
 var p1Score=0;
 var p2Score=0;
 var gameOver = false;
@@ -11,7 +15,8 @@ p1Button.addEventListener("click",function(){
 	if(!gameOver){
 		p1Score++;
 		if(p1Score===winningScore){
-			gameOver = true;	
+			p1Display.classList.add("winner");	
+			gameOver = true;
 		}
 		p1Display.textContent = p1Score;
 	}
@@ -21,8 +26,30 @@ p2Button.addEventListener("click",function(){
 	if(!gameOver){
 		p2Score++;
 		if(p2Score===winningScore){
+			p2Display.classList.add("winner");
 			gameOver= true;
 		}
 		p2Display.textContent = p2Score;
 	}
 });
+
+resetButton.addEventListener("click",function(){
+	reset();
+});
+
+function reset(){
+	p1Score=0;
+	p2Score=0;
+	p1Display.textContent = 0;
+	p2Display.textContent = 0;
+	p1Display.classList.remove("winner");
+	p2Display.classList.remove("winner");
+	gameOver = false;
+}
+
+numInput.addEventListener("change", function(){
+	winningScoreDisplay.textContent = this.value;
+	winningScore = Number(this.value);
+	reset();
+});
+
